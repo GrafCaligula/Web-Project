51b446ee0a62435d43919ce7f5aa26cd4ad18889
Markus <markus.kloepper@gmx.net>
2022-10-27 15:35:38 +0200

    implemted results

--- js/results.js
+++ js/results.js
@@ -1,11 +1,14 @@
 let search = window.location.search;
 let params = new URLSearchParams(search);
 let dataObj = [];
+
 params.forEach((value, key) => {
   // Konsolenausgabe
-  console.log(key + " : " + value);
+  // console.log(key + " : " + value);
+
   // Ausgabe auf HTML-Seite
-  document.write("<p style='font-size: 1.5rem'>" + key + " : " + value + "</p>");
+    // document.write("<p style='font-size: 1.5rem'>" + key + " : " + value + "</p>");
+
   // Ein Array mit key-value-Objekten erstellen
   dataObj.push({ key, value });
 });
